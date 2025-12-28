11bf817149d5701010193210146c63f84b4f207c
devrj29 <bmd.devendra@gmail.com>
2025-12-28 18:09:48 +0530

    Added logger

--- src/main/java/com/demo/controller/CheckoutController.java
+++ src/main/java/com/demo/controller/CheckoutController.java
@@ -6,6 +6,7 @@ import com.demo.exception.InvalidInputException;
 import com.demo.service.ICheckoutService;
 import com.demo.response.dto.BasketPricingResponseDto;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/checkout")
+@Slf4j
 public class CheckoutController {
 
     @Autowired
@@ -20,6 +22,7 @@ public class CheckoutController {
 
     @PostMapping("/basket/calculate")
     public ResponseEntity<?> calculateBasketPrice(@RequestBody @Valid BasketDto basketDto) {
+        log.info("Received request to calculate basket price. BasketId={}", basketDto.getBasketId());
         BasketPricingResponseDto calculatedBasketPrice = groceryServiceImpl.calculateBasketPrice(basketDto);
         String basketPricingDetails = groceryServiceImpl.formatResponse(calculatedBasketPrice);
         return new ResponseEntity<>(basketPricingDetails, HttpStatus.OK);
